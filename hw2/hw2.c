c12ad03c0ad2aacddc6988c7dcce9f329c5e4adf
jmbhughes <hughes.jmb@gmail.com>
2018-08-31 14:37:21 -0600

    basic version works

--- hw2/hw2.c
+++ hw2/hw2.c
@@ -1,18 +1,48 @@
 #include "CSCIx229.h"
 #include <stdio.h>
 
-/*  Lorenz Parameters  */
+//-------------------------------------------------
+// GLOBALS
+// Lorenz Parameters 
 double s  = 10.0;
 double b  = 2.6666;
-double r  = 28.0;
+double r  = 30.0;
 
 double x = 1.0;
-double y = 1.026;
-double z = 0.998;
+double y = 1.0;
+double z = 1.0;
 
 double dt = 0.001;
-double scale = 0.03;
+double scale = 0.01;
 double offset = 1.0;
+int iterations = 500000;
+
+// Viewing
+int th=0;       // Azimuth of view angle
+int ph=0;       // Elevation of view angle
+int viewStep=5;
+//-------------------------------------------------
+
+
+/*
+ *  Convenience routine to output raster text
+ *  Use VARARGS to make this more flexible
+ * taken from Schreduer example 5
+ */
+#define LEN 8192  // Maximum length of text string
+void Print(const char* format , ...)
+{
+   char    buf[LEN];
+   char*   ch=buf;
+   va_list args;
+   //  Turn the parameters into a character string
+   va_start(args,format);
+   vsnprintf(buf,LEN,format,args);
+   va_end(args);
+   //  Display the characters one at a time at the current raster position
+   while (*ch)
+      glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18,*ch++);
+}
 
 /*
  * Calcuates the location of the point after the next Lorenz iteration
@@ -30,29 +60,51 @@ void stepLorenz() {
  *  Function to print any errors encountered
  *  copied from Schreuder's example 5
  */
-void ErrCheck(const char* where)
-{
+void ErrCheck(const char* where) {
    int err = glGetError();
    if (err) fprintf(stderr,"ERROR: %s [%s]\n",gluErrorString(err),where);
+}
+
+void drawLorenzCurve() {
+    x = 1.0;
+    y = 1.0;
+    z = 1.0;
+
+    glBegin(GL_LINE_STRIP);
+    for (int i = 0; i < iterations; i++) {
+        float fractionDrawn = (float) i / (float) iterations;
+        glColor3f(1.0 * fractionDrawn, 0.0, 1.0 * (1 - fractionDrawn));
+        glVertex3f(scale*x, scale*y, scale*z);
+        stepLorenz();
+    }
+    glEnd();
 }
 
 /*
  * function is called by GLUT to display a scene
  */
 void display() {
-   //  Clear screen
-   glClear(GL_COLOR_BUFFER_BIT);
+   // Clear screen
+   glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
 
-   //  Draw triangle
-   stepLorenz();
-   glPointSize(5.0);
-   glEnable(GL_POINT_SMOOTH);
-   glBegin(GL_POINTS);
-   glVertex3f(scale*x, scale*y, scale*y);
-   glEnd();
+   // Reset previous transforms
+   glLoadIdentity();
 
-   //  Make scene visible
+   // Set view angle
+   glRotated(ph,1,0,0);
+   glRotated(th,0,1,0);
+ 
+   // Draw path
+   drawLorenzCurve();
+
+   // Print parameter settings at bottom left of screen
+   glColor3f(1.0, 1.0, 1.0);
+   glWindowPos2i(5,5);
+   Print("s=%5.2f, b=%5.2f, r=%5.2f", s, b, r);
+
+   // Update the display
    glFlush();
+   glutSwapBuffers();
 }
 
 /*
@@ -77,17 +129,48 @@ void reshape(int width,int height)
    glLoadIdentity();
 }
 
+/*
+ *  GLUT calls this routine when an arrow key is pressed
+ *  copied from Schreuder's example 6
+ */
+void special(int key, int x, int y) {
+   //  Right arrow key - increase azimuth by 5 degrees
+   if (key == GLUT_KEY_RIGHT)
+      th += viewStep;
+   //  Left arrow key - decrease azimuth by 5 degrees
+   else if (key == GLUT_KEY_LEFT)
+      th -= viewStep;
+   //  Up arrow key - increase elevation by 5 degrees
+   else if (key == GLUT_KEY_UP)
+      ph += viewStep;
+   //  Down arrow key - decrease elevation by 5 degrees
+   else if (key == GLUT_KEY_DOWN)
+      ph -= viewStep;
+   //  Keep angles to +/-360 degrees
+   th %= 360;
+   ph %= 360;
+   //  Tell GLUT it is necessary to redisplay the scene
+   glutPostRedisplay();
+}
+
+void key(unsigned char ch, int x, int y) {
+    if (ch == '+') {
+        r += 0.1;
+    } else if (ch == '-') {
+        r -= 0.1;
+    }
+    glutPostRedisplay();
+}
+
 int main(int argc, char *argv[]) {
     
    //  Initialize GLUT and process user parameters
    glutInit(&argc,argv);
-
-    
-   //  Request double buffered, true color window 
-   // glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
+ 
+   glutInitDisplayMode(GLUT_RGB | GLUT_DEPTH | GLUT_DOUBLE);
    
    //  Request 500 x 500 pixel window
-   // glutInitWindowSize(500,500);
+   glutInitWindowSize(500,500);
    
    //  Create the window
    glutCreateWindow("HW 2: James Marcus Hughes");
@@ -99,13 +182,13 @@ int main(int argc, char *argv[]) {
    glutReshapeFunc(reshape);
    
    //  Tell GLUT to call "special" when an arrow key is pressed
-   // glutSpecialFunc(special);
+   glutSpecialFunc(special);
    
    //  Tell GLUT to call "key" when a key is pressed
-   // glutKeyboardFunc(key);
+   glutKeyboardFunc(key);
    
    //  Tell GLUT to call "idle" when nothing else is going on
-   glutIdleFunc(display);
+   // glutIdleFunc(display);
 
    //  Pass control to GLUT so it can interact with the user
    ErrCheck("init");
